31bc0e1093ec09af013b6bfbba33d142b3a9f03d
Zero7860 <aspect780@gmail.com>
2025-02-20 11:45:42 +0800

    ddd

--- Coding art.py
+++ Coding art.py
@@ -45,7 +45,7 @@ red_pixels = [(-100, 200), (-75, 175),(-100,175),
               (-75,200),(-100,150),(-125,175),(-125,150),
               (-150,150),(-150,125),(-125,125),(-75,75),
               (-50,75),(-50,50),(-75,50),(-75,25),(-50,25),
-              (-75,0),(-50,0),(0,-150),(25,-175),]
+              (-75,0),(-50,0),(0,-150),(25,-175),(25,-125)]
 
 tom.pu()
 tom.goto(-175,100)
@@ -335,7 +335,18 @@ tom.end_fill()
 black_pixels = [(-75,-25),(-75,-50),(-25,-150),(-25,-175),
                 (0,-125),(25,-100),(25,-75),(-75,-175),
                 (-75,-150),(-125,25),(-150,0),(-100,50),(-125,0),
-                (-100,-125),(-175,-125),(25,-250),(125,-250),(100,-225)]
+                (-100,-125),(-175,-125),(25,-250),(125,-250),
+                (100,-225),(175,-125),(200,-100),(225,-75),(225,-50),
+                (225,50),(200,75),(175,75),(150,100),(125,0),
+                (125,25),(100,50),(75,75),(50,100),(125,100),
+                (100,125),(75,250),(50,225),(100,-25),(100,-50),
+                (75,-25),(50,0),(25,0),(0,25),(-50,100),(-75,125),
+                (-100,125),(-75,150),(-50,175),(-50,200),(-75,225),
+                (-100,225),(-125,200),(-150,175),(-175,150),(-175,125),
+                (-200,100),(-225,75),(-225,50),(-250,25),(-275,0),
+                (-275,-25),(-250,-50),(-225,-75),(-200,-75),
+                (-250,-100),(-275,-125),(-275,-150),(-250,-175),
+                (-225,-175),(-200,-150),(-100,-200),(-100,-175),(-125,-200)]
 
 for x, y in black_pixels:
     draw_black_square(x, y)
@@ -408,9 +419,253 @@ for i in range(8):
     tom.fd(25)
 tom.end_fill()
 
+tom.pu()
+tom.goto(100,-225)
+tom.pd()
+
+tom.begin_fill()
+for i in range(6):
+    tom.color('grey',('#ed1c24'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(125,-175)
+tom.pd()
+
+tom.begin_fill()
+for i in range(6):
+    tom.color('grey',('#ed1c24'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(150,-125)
+tom.pd()
+
+tom.begin_fill()
+for i in range(8):
+    tom.color('grey',('#ed1c24'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(175,-125)
+tom.pd()
+
+tom.begin_fill()
+for i in range(7):
+    tom.color('grey',('#ed1c24'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(200,-100)
+tom.pd()
+
+tom.begin_fill()
+for i in range(6):
+    tom.color('grey',('#ed1c24'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(225,-50)
+tom.pd()
+
+tom.begin_fill()
+for i in range(3):
+    tom.color('grey',('#ed1c24'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(125,-250)
+tom.pd()
+
+tom.begin_fill()
+for i in range(3):
+    tom.color('grey',('#000000'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(150,-200)
+tom.pd()
+
+tom.begin_fill()
+for i in range(3):
+    tom.color('grey',('#000000'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(250,-75)
+tom.pd()
+
+tom.begin_fill()
+for i in range(4):
+    tom.color('grey',('#000000'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(25,100)
+tom.pd()
+
+tom.begin_fill()
+for i in range(4):
+    tom.color('grey',('#000000'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(125,25)
+tom.pd()
+
+tom.begin_fill()
+for i in range(2):
+    tom.color('grey',('#ed1c24'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(100,50)
+tom.pd()
+
+tom.begin_fill()
+for i in range(2):
+    tom.color('grey',('#ed1c24'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(125,125)
+tom.pd()
+
+tom.begin_fill()
+for i in range(2):
+    tom.color('grey',('#000000'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(100,175)
+tom.pd()
+
+tom.begin_fill()
+for i in range(2):
+    tom.color('grey',('#000000'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(75,75)
+tom.pd()
+
+tom.begin_fill()
+for i in range(2):
+    tom.color('grey',('#ffff00'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.begin_fill()
+for i in range(4):
+    tom.color('grey',('#f7941d'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(100,125)
+tom.pd()
+
+tom.begin_fill()
+for i in range(2):
+    tom.color('grey',('#f7941d'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(50,100)
+tom.pd()
+
+tom.begin_fill()
+for i in range(4):
+    tom.color('grey',('#f7941d'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
+
+tom.pu()
+tom.goto(-25,0)
+tom.pd()
+
+tom.begin_fill()
+for i in range(3):
+    tom.color('grey',('#000000'))
+    for i in range(4):
+        tom.fd(25)
+        tom.rt(90)
+    tom.fd(25)
+tom.end_fill()
 
 
 
-tom.hideturtle
+
+
+tom.hideturtle()
 window.tracer(True)
 turtle.exitonclick()
